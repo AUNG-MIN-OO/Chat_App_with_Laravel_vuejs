f3366c610890f8c98854cb6cb717239b252d58f3
Aung Min Oo <aungminoo1314@gmail.com>
2021-11-10 13:20:14 +0900

    chat app message send completed

---
+++ resources/js/Pages/Chat/Container.vue
@@ -0,0 +1,64 @@
+<template>
+    <app-layout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Chat
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <message-container :messages="messages"/>
+                    <input-message :room="currentRoom"/>
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import AppLayout from '@/Layouts/AppLayout.vue'
+import MessageContainer from "@/Pages/Chat/MessageContainer";
+import InputMessage from "@/Pages/Chat/InputMessage";
+
+export default defineComponent({
+    components: {
+        InputMessage,
+        MessageContainer,
+        AppLayout,
+    },
+    data() {
+        return {
+            chatRooms: [],
+            currentRoom : [],
+            messages : [],
+        }
+    },
+    methods: {
+        getRooms() {
+            axios.get('/chat/rooms')
+            .then(response=>{
+                this.chatRooms = response.data;
+                this.setRoom(response.data[0]);
+            })
+            .catch(error=>console.log(error));
+        },
+        setRoom(room) {
+            this.currentRoom = room;
+            this.getMessages();
+        },
+        getMessages(){
+            axios.get('/chat/room/'+this.currentRoom.id+'/messages')
+            .then(response=>{
+                this.messages = response.data;
+            })
+            .catch(error=>console.log(error));
+        }
+    },
+    created() {
+        this.getRooms();
+    }
+})
+</script>
